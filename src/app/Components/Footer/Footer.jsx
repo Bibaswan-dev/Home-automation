412fab2d3f8dbde0822180d483a904e48ea0c258
Bibaswan Nandi <94172148+Bibaswan03@users.noreply.github.com>
2024-03-17 11:22:27 +0530

    first commit

---
+++ src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-slate-950 shadow-inner shadow-slate-900">
+  <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+    
+    <hr className="my-6 border-purple-500 sm:mx-auto lg:my-8" />
+    <div className="sm:flex sm:items-center mx-auto justify-center ">
+      <span className="text-sm text-gray-500 sm:text-center">
+        © 2024{" "}
+        <a href="/" className="hover:underline">
+          BDP Home Automation
+        </a>
+        . All Rights Reserved.
+      </span>
+    </div>
+  </div>
+</footer>
+
+
+  )
+}
+
+export default Footer
