6d710e2735ac74c3805ba718ab55784d58f8638c
Debangan Bhattacharyya <112092614+debangan03@users.noreply.github.com>
2024-03-17 13:34:50 +0530

    commit

--- src/app/Components/Dashboard/DashBoardMain.jsx
+++ src/app/Components/Dashboard/DashBoardMain.jsx
@@ -1,15 +1,103 @@
-import React from 'react'
-import DashBoardComp from './DashBoardComp'
+import React from "react";
+import DashBoardComp from "./DashBoardComp";
+import { FaFan } from "react-icons/fa";
+import { FaLightbulb } from "react-icons/fa";
+import { GiLightBulb } from "react-icons/gi";
+import { TbAirConditioning } from "react-icons/tb";
 
 function DashBoardMain() {
   return (
-    <div className='mt-10 '>
+    <>
+      <h1 className="text-3xl font-sans mt-10 text-white text-center font-semibold ">
+        Smart Home Controll Panel
+      </h1>
+      <hr className="w-[20%] border-[1px] shadow-xl mx-auto mt-[2px]" />
+      <div className="my-20 px-32 font-sans ">
+        <div className="grid md:grid-cols-4 gap-4 grid-cols-1 ">
+          <div className=" px-4 flex justify-between shadow-lg items-center bg-slate-800/80 rounded-xl h-20 text-white mb-4">
+            <p className="text-xl ">Fan</p>
+            <FaFan />
+          </div>
+          <div className=" px-4 flex justify-between shadow-lg items-center bg-slate-800/80 rounded-xl h-20 text-white mb-4">
+            <p className="text-xl ">Bulb</p>
+            <GiLightBulb />
+          </div>
+          <div className=" px-4 flex justify-between shadow-lg items-center bg-slate-800/80 rounded-xl h-20 text-white mb-4">
+            <p className="text-xl ">Led</p>
+            <FaLightbulb />
+          </div>
+          <div className=" px-4 flex justify-between shadow-lg items-center bg-slate-800/80 rounded-xl h-20 text-white mb-4">
+            <p className="text-xl ">Fan</p>
+            <TbAirConditioning />
+          </div>
+        </div>
+        <div className="w-full mt-10 bg-slate-800/80 rounded-xl py-2 px-4 text-white">
+          <h1 className="text-xl font-semibold my-4">Connection Status</h1>
+          <div className="grid md:grid-cols-4 gap-4 grid-cols-1 ">
+            <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+              <p className="text-xl ">Fan</p>
+              <p>Status :</p>
+            </div>
 
+            <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+              <p className="text-xl ">Fan</p>
+              <p>Status :</p>
+            </div>
+            <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+              <p className="text-xl ">Fan</p>
+              <p>Status :</p>
+            </div>
 
-        
+            <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+              <p className="text-xl ">Fan</p>
+              <p>Status :</p>
+            </div>
+          </div>
+        </div>
 
-    </div>
-  )
+        <div className="w-full mt-10 bg-slate-800/80 rounded-xl py-2 px-4 text-white">
+          <h1 className="text-xl font-semibold my-4">
+            Device Details and Action
+          </h1>
+          <div>
+            <div className="relative">
+              <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+                <p className="text-xl ">Fan</p>
+                <p>Status :</p>
+              </div>
+              <button className="absolute bottom-6 bg-blue-500 p-2 rounded right-4">Adjust Speed</button>
+            </div>
+
+            <div className="relative">
+              <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+                <p className="text-xl ">Fan</p>
+                <p>Status :</p>
+              </div>
+              <button className="absolute bottom-6 bg-blue-500 p-2 rounded right-4">Adjust Speed</button>
+            </div><div className="relative">
+              <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+                <p className="text-xl ">Fan</p>
+                <p>Status :</p>
+              </div>
+              <button className="absolute bottom-6 bg-blue-500 p-2 rounded right-4">Adjust Speed</button>
+            </div><div className="relative">
+              <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+                <p className="text-xl ">Fan</p>
+                <p>Status :</p>
+              </div>
+              <button className="absolute bottom-6 bg-blue-500 p-2 rounded right-4">Adjust Speed</button>
+            </div><div className="relative">
+              <div className=" px-4 py-4 justify-between shadow-lg items-center bg-slate-700/80 rounded-xl h-20 text-white mb-4">
+                <p className="text-xl ">Fan</p>
+                <p>Status :</p>
+              </div>
+              <button className="absolute bottom-6 bg-blue-500 p-2 rounded-lg right-4">Adjust Speed</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default DashBoardMain
+export default DashBoardMain;
--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { CgMenuGridO, CgClose } from "react-icons/cg";
 function Navbar() {
   const [mobile, setmobile] = useState(false);
   return (
-    <nav className="bg-[#fdf9e8] shadow-sm py-2 z-30">
+    <nav className="bg-[#070911] shadow-sm py-2 z-30">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div
@@ -18,15 +18,15 @@ function Navbar() {
           >
             <button
               type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-slate-400 hover:bg-transparent hover:text-black focus:outline-none  "
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-slate-400 hover:bg-transparent hover:text-white focus:outline-none  "
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
 
-              {!mobile && <CgMenuGridO className="text-xl text-black" />}
-              {mobile && <CgClose className="text-xl text-black" />}
+              {!mobile && <CgMenuGridO className="text-xl text-white" />}
+              {mobile && <CgClose className="text-xl text-white" />}
 
               <svg
                 className="hidden h-6 w-6"
@@ -46,7 +46,7 @@ function Navbar() {
           </div>
           <div className="flex flex-1 relative items-center justify-center sm:items-stretch sm:justify-start">
             <Link href={"/"} className="flex flex-shrink-0 items-center">
-              <h1 className="text-[#181e3a]  font-sans text-2xl cursor-pointer">
+              <h1 className="text-white  font-sans text-2xl cursor-pointer">
                 BDP Automate
               </h1>
             </Link>
@@ -54,19 +54,19 @@ function Navbar() {
               <div className="flex items-center space-x-4">
                 <Link
                   href={"/About"}
-                  className="text-black hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-white hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   About Us
                 </Link>
                 <Link
                   href={"/Manual"}
-                  className="text-black hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-white hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   Manual
                 </Link>
                 <Link
                   href={"/FeedBack"}
-                  className="text-black hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-white hover:underline hover:underline-offset-4  rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   Feedback
                 </Link>
@@ -81,19 +81,19 @@ function Navbar() {
           <div className="space-y-1 px-2 pb-3 pt-2">
             <Link
               href={"/About"}
-              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
+              className="text-white hover:bg-[#D6F8E7] hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               About Us
             </Link>
             <Link
               href={"/ContactUs"}
-              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
+              className="text-white hover:bg-[#D6F8E7] hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               ContactUs
             </Link>
             <Link
               href={"/FeedBack"}
-              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
+              className="text-white hover:bg-[#D6F8E7] hover:text-white block rounded-md px-3 py-2 text-base font-medium"
             >
               Feedback
             </Link>
