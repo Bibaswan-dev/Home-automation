88456df56fdfdcf9ff7c3d1613158bb09205bda0
Bibaswan Nandi <94172148+Bibaswan03@users.noreply.github.com>
2024-03-17 11:47:46 +0530

    commit

--- src/app/Components/Footer/Footer.jsx
+++ src/app/Components/Footer/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="bg-slate-950 shadow-inner shadow-slate-900">
+    <footer className="bg-[#070911] shadow-inner shadow-slate-900">
   <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     
-    <hr className="my-6 border-purple-500 sm:mx-auto lg:my-8" />
+    <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
     <div className="sm:flex sm:items-center mx-auto justify-center ">
-      <span className="text-sm text-gray-500 sm:text-center">
+      <span className="text-sm text-[#7abdff] sm:text-center">
         © 2024{" "}
-        <a href="/" className="hover:underline">
+        <a href="/" className="hover:underline hover:text-white">
           BDP Home Automation
         </a>
         . All Rights Reserved.
--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { CgMenuGridO, CgClose } from "react-icons/cg";
 function Navbar() {
   const [mobile, setmobile] = useState(false);
   return (
-    <nav className="bg-[#eff8f3] shadow-md   py-2 z-30">
+    <nav className="bg-[#fdf9e8] shadow-sm py-2 z-30">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div
@@ -18,15 +18,15 @@ function Navbar() {
           >
             <button
               type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-slate-400 hover:bg-transparent hover:text-slate-900 focus:outline-none  "
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-slate-400 hover:bg-transparent hover:text-black focus:outline-none  "
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
 
-              {!mobile && <CgMenuGridO className="text-xl text-slate-900" />}
-              {mobile && <CgClose className="text-xl text-slate-900" />}
+              {!mobile && <CgMenuGridO className="text-xl text-black" />}
+              {mobile && <CgClose className="text-xl text-black" />}
 
               <svg
                 className="hidden h-6 w-6"
@@ -44,29 +44,29 @@ function Navbar() {
               </svg>
             </button>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+          <div className="flex flex-1 relative items-center justify-center sm:items-stretch sm:justify-start">
             <Link href={"/"} className="flex flex-shrink-0 items-center">
-              <h1 className="text-slate-700 font-semibold text-xl cursor-pointer">
-               <span className="text-teal-500 "> Saurah</span>.<span>Finance</span>
+              <h1 className="text-[#181e3a]  font-sans text-2xl cursor-pointer">
+                BDP Automate
               </h1>
             </Link>
-            <div className="hidden sm:ml-6 sm:block">
+            <div className="absolute lg:block hidden right-[35%] ">
               <div className="flex items-center space-x-4">
                 <Link
                   href={"/About"}
-                  className="text-slate-700 hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-black hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   About Us
                 </Link>
                 <Link
                   href={"/ContactUs"}
-                  className="text-slate-700 hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-black hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   Contact Us
                 </Link>
                 <Link
                   href={"/FeedBack"}
-                  className="text-slate-700 hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
+                  className="text-black hover:scale-95 hover:text-slate-500 rounded-md px-3 py-2 text-[1rem] font-medium"
                 >
                   Feedback
                 </Link>
@@ -81,19 +81,19 @@ function Navbar() {
           <div className="space-y-1 px-2 pb-3 pt-2">
             <Link
               href={"/About"}
-              className="text-slate-700 hover:bg-[#D6F8E7] hover:text-slate-900 block rounded-md px-3 py-2 text-base font-medium"
+              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
             >
               About Us
             </Link>
             <Link
               href={"/ContactUs"}
-              className="text-slate-700 hover:bg-[#D6F8E7] hover:text-slate-900 block rounded-md px-3 py-2 text-base font-medium"
+              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
             >
               ContactUs
             </Link>
             <Link
               href={"/FeedBack"}
-              className="text-slate-700 hover:bg-[#D6F8E7] hover:text-slate-900 block rounded-md px-3 py-2 text-base font-medium"
+              className="text-black hover:bg-[#D6F8E7] hover:text-black block rounded-md px-3 py-2 text-base font-medium"
             >
               Feedback
             </Link>
@@ -101,7 +101,7 @@ function Navbar() {
         </div>
       )}
 
-      <Link href={'/Signup'} className="absolute top-6 bg-teal-500 text-white font-semibold md:text-lg rounded-full px-5 p-1 right-2 md:right-20 ">Login</Link>
+      
     </nav>
   );
 }
